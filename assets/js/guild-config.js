fd7237bd2c2ed71b08d4a27c7710e8174d396faa
unknown <r_takebe@jyoho.saitama-doken.or.jp>
2026-06-29 20:32:40 +0900

    Configure DOKEN guild cloud API

--- assets/js/guild-config.js
+++ assets/js/guild-config.js
@@ -1,4 +1,4 @@
 window.DOKEN_GUILD_CONFIG = {
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxSyE47dgEqRzugYnufktKxlCx5KMrU_DNDovymGrDX4XPwRhPGrxZbGMpr885WHO2O/exec',
   notifyEmail: 'ageoina@saitama-doken.or.jp'
 };
