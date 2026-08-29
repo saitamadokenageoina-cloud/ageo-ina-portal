cec766c46b2584f2b58c6daeb898b6af34262034
saitamadokenageoina-cloud <saitamadokenageoina@gmail.com>
2026-08-29 21:27:54 +0900

    fix: version home styles with app release

--- assets/js/home-modern.js
+++ assets/js/home-modern.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
   var home=document.createElement('script');
-  home.src='assets/js/home-modern-legacy-v177.js?v=182';
+  home.src='assets/js/home-modern-legacy-v177.js?v=183';
   document.body.appendChild(home);
 })();
