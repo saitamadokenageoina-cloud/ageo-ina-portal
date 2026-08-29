8deb5ad7619f2eb16c1989a147c30af156e87693
saitamadokenageoina-cloud <saitamadokenageoina@gmail.com>
2026-08-29 21:23:03 +0900

    fix: align home rails and card typography

--- assets/js/home-modern.js
+++ assets/js/home-modern.js
@@ -1,6 +1,6 @@
 (function(){
   'use strict';
   var home=document.createElement('script');
-  home.src='assets/js/home-modern-legacy-v177.js?v=181';
+  home.src='assets/js/home-modern-legacy-v177.js?v=182';
   document.body.appendChild(home);
 })();
