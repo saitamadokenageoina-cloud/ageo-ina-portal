180c1696ffb18cd796255a24819c7a6622aa4c48
saitamadokenageoina-cloud <saitamadokenageoina@gmail.com>
2026-08-30 18:26:32 +0900

    既存キャッシュ版数との整合を維持

--- assets/js/home-modern.js
+++ assets/js/home-modern.js
@@ -2,9 +2,9 @@
   'use strict';
   var home=document.createElement('script');
   var palette=document.createElement('link');
-  home.src='assets/js/home-modern-legacy-v177.js?v=188';
+  home.src='assets/js/home-modern-legacy-v177.js?v=186';
   palette.rel='stylesheet';
-  palette.href='assets/css/home-semantic-palette.css?v=188';
+  palette.href='assets/css/home-semantic-palette.css?v=186';
   document.body.appendChild(home);
   document.head.appendChild(palette);
 })();
